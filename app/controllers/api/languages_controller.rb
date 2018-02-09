62827960c8235fa513b021e3edb66ebe6d052951
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2018-02-10 01:44:55 +0200

    Get authorization token from an environment variable.

--- app/controllers/api/languages_controller.rb
+++ app/controllers/api/languages_controller.rb
@@ -1,28 +1,28 @@
 class Api::LanguagesController < ApplicationController
 
+  TOKEN = ENV['GITHUB_TOKEN'].freeze
+
   def index
+    puts "Using authorization token: #{TOKEN}..."
     organization = 'skroutz'
-    uri = URI.parse("https://api.github.com/orgs/#{organization}/repos?type=source")
+    repos_hash = perform_http_request "https://api.github.com/orgs/#{organization}/repos?type=source"
+    langs_hash = {}
+    repos_hash.each do |repo|
+      lang_usage = perform_http_request repo['languages_url']
+      index = repo['name']
+      langs_hash[index] = lang_usage
+    end
+    render json: langs_hash
+  end
+
+  def perform_http_request(url)
+    uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(uri.request_uri)
-    request['Authorization'] = 'token 107f96bacfa3fe50ba6295cd7a10c8eed7c27007'
+    request['Authorization'] = "token #{TOKEN}"
     response = http.request(request)
-    repos_hash = JSON.parse response.body
-    langs_hash = {}
-    repos_hash.each do |repo|
-      uri = URI.parse(repo['languages_url'])
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      request = Net::HTTP::Get.new(uri.request_uri)
-      request['Authorization'] = 'token 107f96bacfa3fe50ba6295cd7a10c8eed7c27007'
-      response = http.request(request)
-      lang_usage = JSON.parse response.body
-      index = repo['name']
-      langs_hash[index] = lang_usage
-      # puts "#{repo['name']} --> #{lang_usage}"
-    end
-    render json: langs_hash
+    JSON.parse response.body
   end
 
 end
