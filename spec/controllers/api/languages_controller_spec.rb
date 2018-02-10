9a4c72a7f6e07fc493e2ef8980cc2a5d81973d30
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2018-02-10 18:59:52 +0200

    One more test.

--- spec/controllers/api/languages_controller_spec.rb
+++ spec/controllers/api/languages_controller_spec.rb
@@ -20,6 +20,12 @@ describe 'API testing', type: :request do
     expect(json['error']).to eq('Organization not found.')
   end
 
+  it 'should return that no auth token was provided' do
+    get '/languages', params: {format: 'json', organization: 'google'}, headers: {}
+    json = JSON.parse response.body
+    expect(json['error']).to eq('No authorization token provided. Visit https://github.com/settings/tokens to get one.')
+  end
+
   it 'should return 200 OK' do
     get '/languages', params: {format: 'json', organization: 'skroutz'}, headers: {Authorization: "token #{ENV['GITHUB_TOKEN']}"}
     expect(response.status).to eq(200)
