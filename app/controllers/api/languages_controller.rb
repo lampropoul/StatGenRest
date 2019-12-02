b66d448597065320181c16f661d1163e1c22ddc0
Vassilis Lambropoulos <lampropoulosv@kotsovolos.gr>
2019-12-02 17:26:25 +0200

    Refactoring.

--- app/controllers/api/languages_controller.rb
+++ app/controllers/api/languages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::LanguagesController < ApplicationController
   include Api::LanguagesHelper
 
@@ -6,8 +8,12 @@ class Api::LanguagesController < ApplicationController
     render_error_no_auth_token && return if @@token.nil? || @@token == ''
     # puts "Using authorization: #{@@token}..."
     organization = params['organization']
-    render_error_no_org_provided && return if organization.nil? || organization == ''
-    response = perform_http_request "https://api.github.com/orgs/#{organization}/repos?type=source", @@token
+    if organization.nil? || organization == ''
+      render_error_no_org_provided
+      return
+    end
+    response = perform_http_request 'https://api.github.com/orgs/'\
+                                    "#{organization}/repos?type=source", @@token
     render_error_not_found && return if response.code == :not_found.to_s
     render_error_unauthorized && return if response.code == :unauthorized.to_s
     repos = JSON.parse response.body
@@ -21,34 +27,34 @@ class Api::LanguagesController < ApplicationController
       # an entry should be something like {"skroutz.rb" => {"Ruby"=>58462}}
       projects_to_langs[repo_name] = repo_lang_usage
     end
-    langs_to_bytes = {}
+    langs_bytes = {}
     all_repos_total_bytes = 0
     projects_to_langs.each do |project, languages|
       puts "#{project} has #{languages}"
       puts '##############################################'
       # iterate over each lang to sum its repo_bytes on all repos
       languages.each do |lang, repo_bytes|
-        if langs_to_bytes[lang].nil?
+        if langs_bytes[lang].nil?
           # if this lang is not in the hash, assign the first value
-          langs_to_bytes[lang] = repo_bytes
+          langs_bytes[lang] = repo_bytes
         else
           # else sum it up
-          langs_to_bytes[lang] += repo_bytes
+          langs_bytes[lang] += repo_bytes
         end
         # sum total repo_bytes from all languages and repos
         all_repos_total_bytes += repo_bytes
       end
     end
-    # reverse sort by repo_bytes of each lang in order to have the most used on top
-    langs_to_bytes_sorted = langs_to_bytes.sort_by { |lang, bytes| bytes }.reverse
+    # reverse sort by repo_bytes of each lang
+    # in order to have the most used on top
+    langs_bytes_sorted = langs_bytes.sort_by { |_lang, bytes| bytes }.reverse
     puts "Total repo_bytes: #{all_repos_total_bytes}"
     langs_to_percentage = {}
-    langs_to_bytes_sorted.each do |lang, bytes|
+    langs_bytes_sorted.each do |lang, bytes|
       # convert all repo_bytes to float in order to get a number below 1,
       # then multiply with 100 to get a percentage
       gather(all_repos_total_bytes, bytes, lang, langs_to_percentage)
     end
     render json: JSON.pretty_generate(langs_to_percentage)
   end
-
 end
